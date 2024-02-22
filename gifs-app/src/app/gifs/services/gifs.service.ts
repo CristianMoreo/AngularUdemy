fc427537d01228b6899a93dbc24d7ae4e6a18e4e
Cristian Moreno <morenoc225@gmail.com>
2024-02-21 22:42:22 -0500

    avances consumo api

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +8,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagHistory :string[] = [];
-  constructor() { }
+  private apiKey:string = 'U8krodg6F531QCWFs0ZCBFVVzjLrCJuy';
+  public gifList:any;
+
+  constructor(private http:HttpClient) { }
 
   get tagHistory(){
     return [...this._tagHistory]
@@ -17,16 +22,30 @@ export class GifsService {
 
     if (this._tagHistory.includes(tag)) {
 
-      this._tagHistory = this._tagHistory.filter
-      
+      this._tagHistory = this._tagHistory.filter((oldTag) =>oldTag !== tag);
+
+      this._tagHistory.unshift(tag);
+      this._tagHistory = this.tagHistory.splice(0,10);
     }
   }
 
-  searchtag( tag:string):void{
+  searchtag( tag:string):void{  
     if (tag.length === 0) {
       return;
     }
-    this._tagHistory.unshift(tag)
+    this.organizeHistory(tag);
+    this._tagHistory.unshift(tag);
+
+    this.http.get('https://api.giphy.com/v2/emoji?api_key=U8krodg6F531QCWFs0ZCBFVVzjLrCJuy&limit=10&offset=0')
+      .subscribe( resp => {
+        // Toca validar porque no me deja acceder a la posicion data 
+        this.gifList = resp
+        console.log(resp);
+        
+      })
+    /*fetch('https://api.giphy.com/v2/emoji?api_key=U8krodg6F531QCWFs0ZCBFVVzjLrCJuy&limit=10&offset=0')
+      .then(resp => resp.json())
+      .then(data => console.log(data))*/
   }
 
 }
