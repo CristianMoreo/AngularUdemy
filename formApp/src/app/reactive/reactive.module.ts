8a5e3d79bf34b0d3f7e5d3b22acea3444bbede35
Cristian Moreno <morenoc225@gmail.com>
2024-02-27 00:18:42 -0500

    Inicios formularios Reactivos

---
+++ formApp/src/app/reactive/reactive.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveRoutingModule } from './reactive-routing.module';
+
+@NgModule({
+  declarations: [
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveRoutingModule
+  ]
+})
+export class ReactiveModule { }
